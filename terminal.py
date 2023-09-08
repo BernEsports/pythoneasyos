2f180e0d320b13422a6df331add2a744f9c58503
BernEsports <liginovr87@gmail.com>
2023-09-08 18:49:57 +0300

    Added arguments taking in main user input

--- terminal.py
+++ terminal.py
@@ -8,10 +8,6 @@ import shutil
 # TODO: Exceptions for every method and help for commands,
 # Cancel method for every func that will make it begin from start
 
-# dir = show directory ; chdir = change directory ; crf = create folder
-# mktxt = make txt file ; rdtxt = read txt file
-
-
 class Terminal:
     def __init__(self):
         self.commands = {
@@ -32,39 +28,43 @@ class Terminal:
         help_text = {
             "help": "Show available commands and their descriptions.",
             "dir": "Show the current working directory.",
-            "chdir": "Change the current working directory.",
-            "crf": "Create a new folder.",
-            "mktxt": "Create a new text file.",
-            "rdtxt": "Read a text file.",
+            "chdir": "Change the current working directory. chdir 'Name\Of\Dir'",
+            "crf": "Create a new folder. crf 'NameOfFolder'",
+            "mktxt": "Create a new text file. mktxt 'txtname'",
+            "rdtxt": "Read a text file. rdtxt 'txtname'",
             "time": "Show current date and time",
-            "rename": "Rename file or folder",
-            "copy": "Copy a file",
-            "delete": "Delete a file",
-            "fsrch": "Search for file"
+            "rename": "Rename file or folder, 'rename test1 test2'",
+            "copy": "Copy a file, 'copy book library'",
+            "delete": "Delete a file, 'delete filename'",
+            "fsrch": "Search for file, 'search filename'"
         }
         for command in self.commands:
             print(f"{command}: {help_text.get(command, 'No description available')}")
-    
 
     def show_dir(self):
         print(os.getcwd())
     
-    def change_dir(self):
-        path_change = input("Enter folder name\n>> ")
+    
+    def change_dir(self, path_change):
         if os.path.exists(path_change) == True:
             os.chdir(path_change)
         else:
             print("Directory doesnt exists")
     
-    def create_folder(self):
-        folder_path = input("Enter folder name\n>> ")
-        if folder_path[0].isnumeric():
-            print("Incorrect name, folder name can't start with a number")
-        elif re.search(r'[!@#$%^&*()_+={}\[\]:;"\'<>,.?/~`\\|]', folder_path):
-            print("Incorrect name, folder name can't start with special character")
-        else:
-            os.makedirs(folder_path)
+    
+    def create_folder(self, folder_path):
+        try:
+            # check if first symbol is number or special symbols in name
+            if folder_path[0].isnumeric():
+                print("Incorrect name, folder name can't start with a number")
+            elif re.search(r'[!@#$%^&*()_+={}\[\]:;"\'<>,.?/~`\\|]', folder_path):
+                print("Incorrect name, folder name can't start with special character")
+            else:
+                os.makedirs(folder_path)
+        except FileExistsError:
+            print("File already exists")
 
+    
     def make_txt(self):
         name_of_txt = input("Enter name of txt file you want to create\n>> ")
         try:
@@ -74,32 +74,32 @@ class Terminal:
         except FileNotFoundError:
             print("This directory not found")
     
-    def read_txt(self):
-        path_of_txt = input("Enter path of txt file you want to read\n>> ")
+    
+    def read_txt(self, path_of_txt):
         try:
             with open(path_of_txt, "r") as f:
-                # TODO: file reading
                 print(f.readline())
                 sleep(5)
         except FileNotFoundError:
             print("This file doesnt exists!")
             
     def time(self):
+        # Days/Month/Year Hour:Minutes
         current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
         print(current_time)
         
-    def rename(self):
-        old_name = input("Enter file name that you want to rename\n>> ")
+    
+    def rename(self, old_name, new_name):
         if os.path.isfile(old_name) == False:
             print("This file is not exists")
-        new_name = input("Enter new name for a file\n>> ")
         if os.path.isfile(new_name):
             print("This file already exists")
         else:
             os.rename(old_name, new_name)
     
-    def fsrch(self):
-        search_query = input("Enter search query (e.g., '*.txt' for text files):\n>> ")
+    
+    def fsrch(self, search_query):
+        # search_query = input("Enter search query (e.g., '*.txt' for text files):\n>> ")
         results = []
 
         for root, dirs, files in os.walk(os.getcwd()):
@@ -114,8 +114,9 @@ class Terminal:
         else:
             print("No files or directories found matching the search criteria.")
     
-    def delete(self):
-        remove_name = input("Enter name of a folder\n>> ")
+    
+    def delete(self, remove_name):
+        # check if file exists and gets user confirm
         if os.path.isfile(remove_name):
             confirm = input(f"You really want to delete '{remove_name}'\
                 ? (Y or N)\n>> ")
@@ -127,45 +128,51 @@ class Terminal:
             print("This file doesnt exists")
     
     
-    def copy(self):
-        copy_from = input("Enter name of a file you want to copy\n>> ")
+    def copy(self, copy_from, copy_to_path):
+        # check if file exists
         if os.path.isfile(copy_from):
-            copy_to_path = input("Enter where do you want to copy this file\n>> ")
             if copy_from in copy_to_path:
                 print("This file already exists")
             shutil.copy(copy_from, copy_to_path)
         else:
             print("This file doesnt exists")
         
-    # TODO: renaming, search directory, deleting, copy files
 
 def main():
+    # single class instance
     terminal = Terminal()
-
     while True:
         try:
-            user_input = input("Enter a command\n"
-                            ">> ")
-            
+            user_input = input("\nEnter a command\n>> ")
+
             if user_input.lower() == "exit":
                 break
-            
-            if user_input in terminal.commands:
-                terminal.commands[user_input]()
-            else:
-                print("Incorrect command, enter \"help\" for list of available commands\n"
-                    "Or enter \"Exit\" to exit terminal")
+
+            # Разделяем ввод пользователя на команду и аргументы
+            command_args = user_input.split()
+
+            if command_args:
+                command = command_args[0].lower()  # Command
+                args = command_args[1:]  # Arguments of method
+
+                if command in terminal.commands:
+                    terminal.commands[command](*args)  # Give args to method
+                else:
+                    print("Incorrect command, enter \"help\" for list of available commands\n"
+                          "Or enter \"Exit\" to exit terminal")
+
         except EOFError:
             print("Exiting...")
             sleep(1)
             break
+
         except KeyboardInterrupt:
             print("Incorrect command!")
+        
+        except TypeError:
+            print("Incorrect args for a command!")
             
-            
-            
-            
-            
+                
 if __name__ == "__main__":
     main()
 
